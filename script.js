67853768484161f067070150a8eb8bf9b42bdafe
Owen Fedchenko <owenfedchenko@gmail.com>
2025-11-05 17:06:43 -0500

    changed link

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const buttons = document.querySelectorAll('section button');
 
   buttons.forEach((btn, index) => {
     btn.addEventListener('click', () => {
-      alert("You clicked a Project!");
+      alert("This will bring you to the project!");
     });
   });
 
 document.getElementById("button1").addEventListener("click", () => {
-    window.location.href = "google.com";
+    window.location.href = "https://owenfeds.github.io/Recipe-Website-Makeover/";
 });
