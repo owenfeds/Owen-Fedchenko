c0b7b46a4a6c5d698ae3f02e773b6a96a0858315
Owen Fedchenko <owenfedchenko@gmail.com>
2025-11-06 15:40:03 -0500

    added more css and html. Fixed js.

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 
-const buttons = document.querySelectorAll('section button');
-
-  buttons.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-      alert("This will bring you to the project!");
-    });
-  });
+document.getElementById('button2').addEventListener('click', () => {
+  alert('');
+});
 
 document.getElementById("button1").addEventListener("click", () => {
     window.location.href = "https://owenfeds.github.io/Recipe-Website-Makeover/";
